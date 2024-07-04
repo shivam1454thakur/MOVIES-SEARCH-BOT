bebab4d3ed11dbe84afd4fdcc52cd1cca7f6994e
shivam1454thakur <146607593+shivam1454thakur@users.noreply.github.com>
2024-07-04 08:57:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -681,13 +681,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/Varagi_shiv'>VJ</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://t.me/findfiles25'>VJ</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://t.me/findfiles25'>VJ Botz</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/findfiles25'>TEAM VJ</a>
 """
 
     RENAME_TXT = """
