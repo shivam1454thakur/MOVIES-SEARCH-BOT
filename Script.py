7fd89709be42859ffb412d1179f078ea3bd6f9ad
shivam1454thakur <146607593+shivam1454thakur@users.noreply.github.com>
2024-07-04 09:57:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -206,7 +206,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : @Varagi_shiv {file_name}
 
 <b> Size ⚙️: {file_size}</b>""" 
 
